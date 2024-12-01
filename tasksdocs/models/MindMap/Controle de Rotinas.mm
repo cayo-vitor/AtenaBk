9d81cfdf5eb7f6e3b28b931b7c79c6377905e9a2
Ecliton <75632668+AnaoTurco@users.noreply.github.com>
2024-12-01 18:31:12 -0300

    Add files via upload

---
+++ tasksdocs/models/MindMap/Controle de Rotinas.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Controle de Rotinas" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1732900468830" STYLE="oval">
+<font NAME="Segoe UI" SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<node POSITION="bottom_or_right" ID="ID_421439084" CREATED="1732900719170" MODIFIED="1732901233779" HGAP_QUANTITY="-109.75 pt" VSHIFT_QUANTITY="-10.5 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Requisitos
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#00007c"/>
+<node TEXT="Autenticação e Registro de Usuários" ID="ID_1179279176" CREATED="1732900917262" MODIFIED="1732901367500" HGAP_QUANTITY="96.5 pt" VSHIFT_QUANTITY="-10.5 pt"/>
+<node TEXT="Criação, edição e exclusão de tarefas." ID="ID_373813344" CREATED="1732900955554" MODIFIED="1732900989176" HGAP_QUANTITY="92 pt" VSHIFT_QUANTITY="4.5 pt"/>
+<node TEXT="Organização de tarefas por categorias." ID="ID_1682487435" CREATED="1732900972058" MODIFIED="1732900991590" HGAP_QUANTITY="93.5 pt" VSHIFT_QUANTITY="8.25 pt"/>
+<node TEXT="Monitoramento de status (pendente, em andamento, concluído)." ID="ID_1972143999" CREATED="1732901002861" MODIFIED="1732901017303" HGAP_QUANTITY="76.25 pt" VSHIFT_QUANTITY="9.75 pt"/>
+</node>
+<node TEXT="Funcionalidades" POSITION="top_or_left" ID="ID_1442461000" CREATED="1732900747195" MODIFIED="1732901058110">
+<edge COLOR="#007c00"/>
+<node TEXT="Gerenciamento de Tarefas" ID="ID_1869133708" CREATED="1732901037424" MODIFIED="1732901058109" VSHIFT_QUANTITY="-60 pt">
+<node TEXT="CRUD de tarefas." ID="ID_371075840" CREATED="1732901067494" MODIFIED="1732901101552" VSHIFT_QUANTITY="-19.5 pt"/>
+<node TEXT="Filtros e ordenações (prioridade, data, status)." ID="ID_571376892" CREATED="1732901086066" MODIFIED="1732901098845" VSHIFT_QUANTITY="5.25 pt"/>
+</node>
+</node>
+<node TEXT="Arquitetura" POSITION="bottom_or_right" ID="ID_1877066322" CREATED="1732900761378" MODIFIED="1732900777849" HGAP_QUANTITY="24.5 pt" VSHIFT_QUANTITY="78 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="RESTful API." ID="ID_1679382904" CREATED="1732901141656" MODIFIED="1732901179509" VSHIFT_QUANTITY="-15.75 pt"/>
+<node TEXT="Banco de dados relacional (MySQL)" ID="ID_958781810" CREATED="1732901158073" MODIFIED="1732901181197" HGAP_QUANTITY="15.5 pt"/>
+<node TEXT="Camadas: Controller, Service, Repository." ID="ID_1073414629" CREATED="1732901195376" MODIFIED="1732901198990" VSHIFT_QUANTITY="10.5 pt"/>
+</node>
+<node TEXT="Tecnologias" POSITION="top_or_left" ID="ID_265851865" CREATED="1732900788471" MODIFIED="1732900800109" VSHIFT_QUANTITY="51.75 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="Backend: Node.js, Express." ID="ID_1253129248" CREATED="1732901325415" MODIFIED="1732901327019"/>
+<node TEXT="Banco de dados: MySQL, MONGODB." ID="ID_1559347975" CREATED="1732901331951" MODIFIED="1733076499486" HGAP_QUANTITY="23.75 pt" VSHIFT_QUANTITY="15.75 pt"/>
+</node>
+<node TEXT="Cronograma" POSITION="bottom_or_right" ID="ID_5283381" CREATED="1732900805074" MODIFIED="1732901356052" HGAP_QUANTITY="-109.75 pt" VSHIFT_QUANTITY="-55.5 pt">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Semana 1: Planejamento e estrutura inicial (rotas, banco) e documentação." ID="ID_1368762112" CREATED="1732901238478" MODIFIED="1732901291174" HGAP_QUANTITY="-303.24999 pt" VSHIFT_QUANTITY="109.5 pt"/>
+<node TEXT="Semana 2: Implementar autenticação e Relatórios." ID="ID_1436208167" CREATED="1732901260939" MODIFIED="1732901312696" HGAP_QUANTITY="19.25 pt" VSHIFT_QUANTITY="-26.25 pt"/>
+</node>
+</node>
+</map>
