bf5963f67bcfda22a615e0da64813055cebf6a02
Ecliton <75632668+AnaoTurco@users.noreply.github.com>
2024-12-08 10:54:46 -0300

    Update userRoutes.js

--- tasksapi/task-api/routes/userRoutes.js
+++ tasksapi/task-api/routes/userRoutes.js
@@ -24,4 +24,38 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        res.status(200).json({ message: 'Usuário excluída com sucesso!' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
+
+router.put('/:id', async (req, res) => {
+    const {  name, email, password } = req.body;
+
+    try {
+        const updatedUser= await User.findByIdAndUpdate(
+            req.params.id,
+            { name, email, password  },  // Atualiza a prioridade também
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
